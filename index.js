4a16befe969b0aa01bf8a2d96a4bc24e87702046
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-29 15:52:28 +0100

    8. Add phoneRegex and isValid function

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const resultsDiv = document.getElementById("results-div");
 
+const phoneRegex = /^1?\s*(\(\d{3}\)|\d{3})[-.\s]?\d{3}[-.\s]?\d{4}$/;
+
+const isValidPhoneNumber = (number) => phoneRegex.test(number);
+
 const validateUserInput = (input) => {
   if (input === "") {
     alert("Please provide a phone number");
     return;
   }
+
+  const isValid = isValidPhoneNumber(input);
+
+  displayResult(
+    ` ${
+      isValid
+        ? "Valid US number: " + `${input}`
+        : "Invalid US number: " + `${input}`
+    }`
+  );
 };
+
 const displayResult = (input) => {
   console.log("Inserting");
-  resultsDiv.innerHTML += `<p>"Valid/Invalid" US number: ${input}</p> `;
+  resultsDiv.innerHTML += `<p>${input}</p> `;
 };
 
 const initEvents = () => {
