5e4fc21dad0c49a800466323774b770a4fdc4303
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-29 10:23:15 +0100

    3. Add function initEvents to check for clicks

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const userInput = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const resultsDiv = document.getElementById("results-div");
+
+const initEvents = () => {
+  checkBtn.addEventListener("click", (e) => {
+    console.log("click on Check", e.target);
+  });
+  clearBtn.addEventListener("click", (e) => {
+    console.log("click on Clear", e.target);
+  });
+};
+initEvents();
